f773ace32991a98016c7fe152ea36ae8da910880
Nurali <sarbakyshnurali@gmail.com>
2021-03-07 03:05:43 +0600

    A warning for a user has been posted

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,6 +1,7 @@
 //a program that takes an operation followed by two operands and outputs the result
 #include <iostream>
 int main() {
+    std::cout<<"We are sorry but this programs only works with 2 numbers at a time, developer promises you we will change it in the future;"<<"\n";
     std::cout<<"Please enter your operation symbol(i.e. *,+,/,-) and two numbers: \n";
     int val1,val2;
     std::string operation;
