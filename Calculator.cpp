27d344d8a14c83d4f0aeeb911afbb9ad9a03f4b0
Nurali <sarbakyshnurali@gmail.com>
2021-03-07 02:10:50 +0600

    New files with different types have been added

---
+++ Calculator.cpp
@@ -0,0 +1,19 @@
+//a program that takes an operation followed by two operands and outputs the result
+#include <iostream>
+int main() {
+    std::cout<<"Please enter your operation symbol(i.e. *,+,/,-) and two numbers: \n";
+    int val1,val2;
+    std::string operation;
+    std::cin>>operation>>val1>>val2;
+    if (operation=="*")
+        std::cout<<val1<<" * "<<val2<<" = "<<val1*val2<<"\n";
+    if (operation=="+")
+        std::cout<<val1<<" + "<<val2<<" = "<<val1+val2<<"\n";
+    if (operation=="-")
+        std::cout<<val1<<" -"<<val2<<" = "<<val1-val2<<"\n";
+    if (operation== "/")
+        std::cout<<val1<<" / "<<val2<<" = "<<val1/val2<<"\n";
+    else
+        std::cout<<"Input is invalid"<<std::endl;
+    return 0;
+}
--- Readme.md
+++ Readme.md
@@ -1,2 +1,3 @@
 # 519030990004 NURALI SARBAKYSH
 # Hey, what's up? This is my Git project, it includes some txt, cpp and odt files and their updates.
+# Hope you enjoy it
